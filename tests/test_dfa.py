55279fff46e6cf8aa8e61924a1227a90dc06dad9
Quentin18 <quentindeschamps18@orange.fr>
2020-11-08 23:31:31 +0100

    add tests init dfa

--- tests/test_dfa.py
+++ tests/test_dfa.py
@@ -6,6 +6,7 @@ https://en.wikipedia.org/wiki/Deterministic_finite_automaton
 """
 import pytest
 from fsmdot.dfa import dfa
+from fsmdot.error import fsmError
 
 
 @pytest.fixture
@@ -33,6 +34,77 @@ def a2():
     q0 = 'S0'
     F = {'S0'}
     return dfa(Q, S, T, q0, F)
+
+
+def test_init_1():
+    with pytest.raises(fsmError):
+        Q = {'S0', 'S1', 'S2'}  # Q must be a list
+        S = ['0', '1']
+        T = [
+            ['S0', 'S1'],
+            ['S2', 'S0'],
+            ['S1', 'S2']
+        ]
+        q0 = 'S0'
+        F = {'S0'}
+        dfa(Q, S, T, q0, F)
+
+
+def test_init_2():
+    with pytest.raises(fsmError):
+        Q = ['S0', 'S1', 'S2']
+        S = {'0', '1'}  # S must be a list
+        T = [
+            ['S0', 'S1'],
+            ['S2', 'S0'],
+            ['S1', 'S2']
+        ]
+        q0 = 'S0'
+        F = {'S0'}
+        dfa(Q, S, T, q0, F)
+
+
+def test_init_3():
+    with pytest.raises(fsmError):
+        Q = ['S0', 'S1', 'S2']
+        S = ['0', '1']
+        T = [
+            ['S0', 'S1'],
+            ['S2', 'S0'],
+            ['S1', 'S2']
+        ]
+        q0 = 'S3'   # Q does not contain q0
+        F = {'S0'}
+        dfa(Q, S, T, q0, F)
+
+
+def test_init_4():
+    with pytest.raises(fsmError):
+        Q = ['S0', 'S1', 'S2']
+        S = ['0', '1']
+        T = [
+            ['S0', 'S1'],
+            ['S2', 'S0'],
+            ['S1', 'S2']
+        ]
+        q0 = 'S0'
+        F = {'S0', 'S3'}    # Q does not contain all states of F
+        dfa(Q, S, T, q0, F)
+
+
+def test_init_5():
+    with pytest.raises(fsmError):
+        Q = [0, 1, 2, 3]
+        S = ['a', 'b']
+        T = [
+            [{0, 1}, {0}],
+            [{2}, {}],
+            [{3}, {}],
+            [{3}, {3}]
+        ]   # The state-transition table is not deterministic
+        q0 = 0
+        F = {3}
+        dfa(Q, S, T, q0, F)
 
 
 def test_delta(a1, a2):
