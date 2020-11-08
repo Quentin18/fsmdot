3d372e3d04777dcb1fac40a71693c51327dc5242
Quentin18 <quentindeschamps18@orange.fr>
2020-11-08 23:24:24 +0100

    add tests dfa

--- tests/test_dfa.py
+++ tests/test_dfa.py
@@ -1,9 +1,15 @@
+"""
+Tests for the dfa class.
+
+Automatons are inspired by Wikipedia:
+https://en.wikipedia.org/wiki/Deterministic_finite_automaton
+"""
 import pytest
 from fsmdot.dfa import dfa
 
 
 @pytest.fixture
-def automata():
+def a1():
     Q = ['S1', 'S2']
     S = ['0', '1']
     T = [
@@ -15,6 +21,40 @@ def automata():
     return dfa(Q, S, T, q0, F)
 
 
-def test_accept(automata):
-    assert not automata.accept('11110')
-    assert automata.accept('110110110101')
+@pytest.fixture
+def a2():
+    Q = ['S0', 'S1', 'S2']
+    S = ['0', '1']
+    T = [
+        ['S0', 'S1'],
+        ['S2', 'S0'],
+        ['S1', 'S2']
+    ]
+    q0 = 'S0'
+    F = {'S0'}
+    return dfa(Q, S, T, q0, F)
+
+
+def test_delta(a1, a2):
+    assert a1.delta('S1', '0') == 'S2'
+    assert a1.delta('S2', '0') == 'S1'
+    assert a1.delta('S1', '1') == 'S1'
+    assert a1.delta('S2', '1') == 'S2'
+
+    assert a2.delta('S0', '0') == 'S0'
+    assert a2.delta('S0', '1') == 'S1'
+    assert a2.delta('S1', '0') == 'S2'
+    assert a2.delta('S1', '1') == 'S0'
+    assert a2.delta('S2', '0') == 'S1'
+    assert a2.delta('S2', '1') == 'S2'
+
+
+def test_accept(a1, a2):
+    assert not a1.accept('11110')
+    assert a1.accept('110110110101')
+
+    assert a2.accept('1001')
+    assert a2.accept('10101')
+    assert a2.accept('11100010100')
+    assert not a2.accept('101')
+    assert not a2.accept('1110')
