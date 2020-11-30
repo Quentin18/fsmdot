a8c8351541b6ba0dde2db79128e1050426ccd259
Quentin18 <quentindeschamps18@orange.fr>
2020-11-30 18:18:14 +0100

    update tests

--- tests/test_dfa.py
+++ tests/test_dfa.py
@@ -6,7 +6,6 @@ https://en.wikipedia.org/wiki/Deterministic_finite_automaton
 """
 import pytest
 from fsmdot.dfa import Dfa
-from fsmdot.error import FsmError
 
 
 @pytest.fixture
@@ -49,77 +48,6 @@ def a2():
     q0 = 'S0'
     F = {'S0'}
     return Dfa(Q, S, d, q0, F)
-
-
-def test_init_1():
-    with pytest.raises(FsmError):
-        Q = {'S0', 'S1', 'S2'}
-        S = {'0', '1'}
-        d = {
-            'S0': {
-                '0': 'S0',
-                '1': 'S1'
-            },
-            'S1': {
-                '0': 'S2',
-                '1': 'S0'
-            },
-            'S2': {
-                '0': 'S1',
-                '1': 'S2'
-            }
-        }
-        q0 = 'S3'   # Q does not contain q0
-        F = {'S0'}
-        Dfa(Q, S, d, q0, F)
-
-
-def test_init_2():
-    with pytest.raises(FsmError):
-        Q = {'S0', 'S1', 'S2'}
-        S = {'0', '1'}
-        d = {
-            'S0': {
-                '0': 'S0',
-                '1': 'S1'
-            },
-            'S1': {
-                '0': 'S2',
-                '1': 'S0'
-            },
-            'S2': {
-                '0': 'S1',
-                '1': 'S2'
-            }
-        }
-        q0 = 'S0'
-        F = {'S0', 'S3'}    # Q does not contain all states of F
-        Dfa(Q, S, d, q0, F)
-
-
-def test_init_3():
-    with pytest.raises(FsmError):
-        Q = {0, 1, 2, 3}
-        S = {'a', 'b'}
-        d = {
-            0: {
-                'a': {0, 1},
-                'b': {0}
-            },
-            1: {
-                'a': {2}
-            },
-            2: {
-                'a': {3}
-            },
-            3: {
-                'a': {3},
-                'b': {3}
-            }
-        }   # the state-transition function is not deterministic
-        q0 = 0
-        F = {3}
-        Dfa(Q, S, d, q0, F)
 
 
 def test_delta(a1, a2):
